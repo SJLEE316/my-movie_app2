d53bb25fcfe6f8704888a267474d23241ce65a5b
SJLEE316 <lsj8249@naver.com>
2020-07-08 14:34:38 +0900

    Updates

---
+++ precache-manifest.b48ec30a56e392edd3511c8aa4b72513.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0917832148270e0cee5bbd2e15b4a02",
+    "url": "/my-movie_app2/index.html"
+  },
+  {
+    "revision": "e11eacd9cb28b16af411",
+    "url": "/my-movie_app2/static/css/main.9b4493a2.chunk.css"
+  },
+  {
+    "revision": "cafa46654d7b01dd01b7",
+    "url": "/my-movie_app2/static/js/2.d1ea1ca4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-movie_app2/static/js/2.d1ea1ca4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e11eacd9cb28b16af411",
+    "url": "/my-movie_app2/static/js/main.edf33d45.chunk.js"
+  },
+  {
+    "revision": "068c2e9c74764b827533",
+    "url": "/my-movie_app2/static/js/runtime-main.d4ade1d8.js"
+  }
+]);
